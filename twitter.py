ce4fd6df272167d6255a2c85b62de2520d838799
Lesyk15 <the.lesyk@gmail.com>
2017-03-08 01:43:24 +0100

    removing credentiaas

--- twitter.py
+++ twitter.py
@@ -4,11 +4,11 @@ import os.path
 
 
 # http://dev.twitter.com/apps/myappid
-CONSUMER_KEY = 'fLXcjwKlFoNcbrZ7rjJjnS12l'
-CONSUMER_SECRET = 'ra7XxLsemjrawZxamJr9e9oFLHNKcp4AkExknt5x0Tb1ItdPgV'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
 # http://dev.twitter.com/apps/myappid/my_token
-ACCESS_TOKEN_KEY= '46417241-QgcltFwzmB7yEsezxE4WD2Ub1z5Msiu61To8POlKY'
-ACCESS_TOKEN_SECRET= 'o87x7kYOB0yIfeZR1nPhjzwxCtGWWDNIoQ1evfEPGfc76'
+ACCESS_TOKEN_KEY= ''
+ACCESS_TOKEN_SECRET= ''
 
 obrazek = ""
 def tweet(status):
